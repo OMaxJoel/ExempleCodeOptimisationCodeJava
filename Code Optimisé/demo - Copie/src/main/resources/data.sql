20d5931c509b89465e64bc2f3da96c4eeeeca71d
OURA MAX-JOEL AFFI <ouramj@gmail.com>
2024-11-22 21:22:48 +0000

    Code final

--- Code Optimisé/demo - Copie/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO product (name, price) VALUES ('Laptop', 999.99);
-INSERT INTO product (name, price) VALUES ('Smartphone', 499.99);
-INSERT INTO product (name, price) VALUES ('Tablet', 299.99);
-INSERT INTO product (name, price) VALUES ('Headphones', 99.99);
-INSERT INTO product (name, price) VALUES ('Smartwatch', 199.99);
